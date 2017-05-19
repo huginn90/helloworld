01cd20510640facaca2ef7a2b9596ff6f806da4e
huginn90 <huginn90@gmail.com>
2017-05-19 17:57:50 +0900

    main, ignore 변경

--- app/src/main/java/com/iot/helloworld/MainActivity.java
+++ app/src/main/java/com/iot/helloworld/MainActivity.java
@@ -11,5 +11,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         System.out.println(1);
+
+        System.out.println("test");
+        
     }
 }
